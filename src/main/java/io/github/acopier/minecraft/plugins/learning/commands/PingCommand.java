dced5d4aa2b9ccea06c6a8ded7e90750f5c2dbc9
acopier <acopierlive@gmail.com>
2025-08-25 14:28:58 +0200

    the first mass surveillance implementation

--- src/main/java/io/github/acopier/minecraft/plugins/learning/commands/PingCommand.java
+++ src/main/java/io/github/acopier/minecraft/plugins/learning/commands/PingCommand.java
@@ -5,6 +5,10 @@ import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 
@@ -15,13 +19,13 @@ public class PingCommand {
 
   public static int commandLogic(CommandContext<CommandSourceStack> ctx) {
     Entity executor = ctx.getSource().getExecutor();
-    Player sender = (Player) ctx.getSource().getSender();
+    Server server = Bukkit.getServer();
 
-    if (!(executor instanceof Player)) {
-      sender.sendMessage("Only players can use /ping!");
+    if (!(executor instanceof Player sender)) {
+      ctx.getSource().getSender().sendMessage("Only players can use /ping!");
       return Command.SINGLE_SUCCESS;
     }
-
+    server.broadcast(Component.text("[SURVEILLANCE] " + sender.getName() + " used /ping").color(TextColor.color(0xAAAAAA)), Server.BROADCAST_CHANNEL_ADMINISTRATIVE);
     sender.sendMessage("Ping: " + sender.getPing());
 
     return Command.SINGLE_SUCCESS;
