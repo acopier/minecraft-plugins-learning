6d180a653f80a3d9a84f31806db206277eaad8c8
acopier <acopierlive@gmail.com>
2025-08-26 09:22:50 +0200

    duh

--- src/main/java/io/github/acopier/minecraft/plugins/learning/commands/PingCommand.java
+++ src/main/java/io/github/acopier/minecraft/plugins/learning/commands/PingCommand.java
@@ -3,8 +3,8 @@ package io.github.acopier.minecraft.plugins.learning.commands;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
-import io.github.acopier.minecraft.plugins.learning.utilities.Checking;
-import io.github.acopier.minecraft.plugins.learning.utilities.Logging;
+import io.github.acopier.minecraft.plugins.learning.utilities.CheckUtility;
+import io.github.acopier.minecraft.plugins.learning.utilities.LogUtility;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
 import org.bukkit.entity.Player;
@@ -17,9 +17,9 @@ public class PingCommand {
   }
 
   public static int commandLogic(CommandContext<CommandSourceStack> ctx) {
-    Player sender = (Player) Checking.getPlayer(ctx, commandName);
+    Player sender = (Player) CheckUtility.getPlayer(ctx, commandName);
 
-    Logging.surveillance(String.format("%s used /%s", sender.getName(), commandName));
+    LogUtility.surveillance(String.format("%s used /%s", sender.getName(), commandName));
     sender.sendMessage("Ping: " + sender.getPing());
 
     return Command.SINGLE_SUCCESS;
--- src/main/java/io/github/acopier/minecraft/plugins/learning/utilities/LogUtility.java
+++ src/main/java/io/github/acopier/minecraft/plugins/learning/utilities/LogUtility.java
@@ -5,7 +5,7 @@ import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.Bukkit;
 import org.bukkit.Server;
 
-public class Logging {
+public class LogUtility {
   public static void surveillance(String text) {
     Server server = Bukkit.getServer();
     server.broadcast(Component.text(String.format("[SURVEILLANCE] | %s", text)).color(TextColor.color(0xAAAAAA)), Server.BROADCAST_CHANNEL_ADMINISTRATIVE);
